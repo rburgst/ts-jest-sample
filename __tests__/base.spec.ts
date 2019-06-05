3d84731c19f54d556815c495a029cefb3c028165
Rainer Burgstaller <rburgst@gmail.com>
2019-06-05 06:57:41 +0200

    upgrade dependencies and demonstrate update snapshots link
    - update snapshots link not working in IntelliJ 2019.2

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -1,32 +1,38 @@
 import { evaluate, BinaryOperation, BinaryOperators } from "../src";
 
 describe("Simple expression tests", () => {
-    test("Check literal value", () => {
-        expect(evaluate({ type: "literal", value: 5 })).toBeCloseTo(5);
-    });
-    test("Check addition", () => {
-        let expr = bin("+", 5, 10);
-        expect(evaluate(expr)).toBeCloseTo(15);
-    });
-    test("Check subtraction", () => {
-        let expr = bin("-", 5, 10);
-        expect(evaluate(expr)).toBeCloseTo(-5);
-    });
-    test("Check multiplication", () => {
-        let expr = bin("*", 5, 10);
-        expect(evaluate(expr)).toBeCloseTo(50);
-    });
-    test("Check division", () => {
-        let expr = bin("/", 10, 5);
-        expect(evaluate(expr)).toBeCloseTo(2);
-    });
+  test("Check literal value", () => {
+    expect(evaluate({ type: "literal", value: 5 })).toBeCloseTo(5);
+  });
+  test("Check addition", () => {
+    let expr = bin("+", 5, 10);
+    expect(evaluate(expr)).toBeCloseTo(15);
+  });
+  test("Check subtraction", () => {
+    let expr = bin("-", 5, 10);
+    expect(evaluate(expr)).toBeCloseTo(-5);
+  });
+  test("Check multiplication", () => {
+    let expr = bin("*", 5, 10);
+    expect(evaluate(expr)).toBeCloseTo(50);
+  });
+  test("Check division", () => {
+    let expr = bin("/", 10, 5);
+    expect(evaluate(expr)).toBeCloseTo(2);
+  });
+  test("check inline snapshot", () => {
+    expect(evaluate({ type: "literal", value: 5 })).toMatchInlineSnapshot(`4`);
+  });
+  test("check external snapshot", () => {
+    expect(evaluate({ type: "literal", value: 5 })).toMatchSnapshot();
+  });
 });
 
 function bin(op: BinaryOperators, x: number, y: number): BinaryOperation {
-    return {
-        type: "binary",
-        operator: op,
-        left: { type: "literal", value: x },
-        right: { type: "literal", value: y },
-    };
+  return {
+    type: "binary",
+    operator: op,
+    left: { type: "literal", value: x },
+    right: { type: "literal", value: y }
+  };
 }
